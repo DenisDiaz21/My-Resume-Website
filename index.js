6ca6b26231dd4b7f251e33a3824039d388b708bf
Denis Diaz <denisdiaz2198@gmail.com>
2020-11-15 18:47:14 -0500

    js/css/html

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-// // Sticky Navbar
-// window.onscroll = function() {stickyNav()};
+// Sticky Navbar
+window.onscroll = function() {stickyNav()};
 
-// let navbar = document.getElementById("navbar");
-// let sticky = navbar.offsetTop;
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
 
-// function stickyNav() 
-// {
-//     if (window.pageYOffset >= sticky)
-//      {
-//         navbar.classList.add("sticky");
-//      }
-//      else 
-//      {
-//         navbar.classList.remove("sticky");
-//      }
-// }
+function stickyNav() 
+{
+    if (window.pageYOffset >= sticky)
+     {
+        navbar.classList.add("sticky");
+     }
+     else 
+     {
+        navbar.classList.remove("sticky");
+     }
+}
