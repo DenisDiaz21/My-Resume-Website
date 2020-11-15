417232357b6dbf132ef78f26e2a4acad92582ebe
Denis Diaz <denisdiaz2198@gmail.com>
2020-11-15 09:25:04 -0500

    re-did html/css - added js file

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// // Sticky Navbar
+// window.onscroll = function() {stickyNav()};
+
+// let navbar = document.getElementById("navbar");
+// let sticky = navbar.offsetTop;
+
+// function stickyNav() 
+// {
+//     if (window.pageYOffset >= sticky)
+//      {
+//         navbar.classList.add("sticky");
+//      }
+//      else 
+//      {
+//         navbar.classList.remove("sticky");
+//      }
+// }
