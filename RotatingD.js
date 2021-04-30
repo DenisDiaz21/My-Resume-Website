04e08b6727b9ab2cb400af7106fc6a300d6097d8
DenisDiaz21 <71994529+DenisDiaz21@users.noreply.github.com>
2021-04-30 18:42:27 -0400

    In this commit, I have added more functionality to the loader intro. I made it fade out better and more fluidly. I also disabled scrolling for 3 seconds in the RotatingD.js file. This is to disable scrolling while the animation occurs.

--- RotatingD.js
+++ RotatingD.js
@@ -1,9 +1,24 @@
+// Loader Rotating D Cancel
 let loader = document.querySelector(".loader-wrapper");
 
 console.log(loader);
 function ass() {
     setTimeout(() => {
-    loader.style.opacity = 0;
+    loader.parentElement.removeChild(loader);
 }, 4000);}
 ass();
 
+// Disable scroll
+function DisableScrolling () {
+window.onscroll = function () { window.scrollTo(0, 0); };
+}
+
+function EnableScrollAgain(){
+    setTimeout(() =>
+    {
+        window.onscroll = function(){};
+    }, 3000);
+
+}
+DisableScrolling();
+EnableScrollAgain();
