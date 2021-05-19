4fceb564b04b69f6fe2ce9852472990b3314529c
DenisDiaz21 <71994529+DenisDiaz21@users.noreply.github.com>
2021-05-19 18:35:27 -0400

    Added animations to the project buttons

--- Scripts/ProjectTabs.js
+++ Scripts/ProjectTabs.js
@@ -14,20 +14,25 @@ projectContent.forEach((e,i) => {
 // Onclick function to display designated project
 button1.onclick = () =>{
     project1.style.display = 'flex';
-    project1.style.width = '100%';
-    project1.style.transition = 'width 2s'
+    project1.style.animation = 'fade-in .5s'
+
     project2.style.display = 'none';
     project3.style.display = 'none';
 
 }
 button2.onclick = () =>{
     project1.style.display = 'none';
+
     project2.style.display = 'flex';
+    project2.style.animation = 'fade-in .5s'
+
     project3.style.display = 'none';
 }
 button3.onclick = () =>{
     project1.style.display = 'none';
     project2.style.display = 'none';
+
     project3.style.display = 'flex';
+    project3.style.animation = 'fade-in .5s';
 }
 
