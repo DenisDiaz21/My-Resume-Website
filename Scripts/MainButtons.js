e3294b8c62a3bf8c64897ea0fbc675cb026f2556
DenisDiaz21 <71994529+DenisDiaz21@users.noreply.github.com>
2021-05-19 18:45:08 -0400

    Changed name of button script and added functionality to both buttons

--- Scripts/MainButtons.js
+++ Scripts/MainButtons.js
@@ -3,3 +3,9 @@ let resumeButton = document.querySelector(".resumeButton");
 resumeButton.onclick = () => {
     window.open('./Denis Diaz Resume.pdf');
 }
+
+let sayHello = document.querySelector(".sayHello");
+
+sayHello.onclick = () => {
+    window.open("mailto:denisdiaz2198@gmail.com")
+}
