4052dd0df6c25fdb3013295b8ca11f96f312a64d
DenisDiaz21 <71994529+DenisDiaz21@users.noreply.github.com>
2021-05-20 19:32:01 -0400

    Modified page load up to include animations. Added and changed classes and formatting to HTML and CSS. Added new JS file AppearingDivs.js to add this new functionality

---
+++ Scripts/AppearingDivs.js
@@ -0,0 +1,59 @@
+let home = document.querySelector(".home");
+let contact = document.querySelector(".contact");
+let bigD = document.querySelector(".headerTitle");
+let childBanner1Heading = document.querySelector(".childBanner1Heading");
+let childBanner1Name = document.querySelector(".childBanner1Name");
+let iDevelop = document.querySelector(".iDevelop");
+let smallBio = document.querySelector(".smallBio");
+let landingPageButtons = document.querySelector(".landingPageButtons");
+let resumeButton2ElectricBoogaloo = document.querySelector(".resumeButton2ElectricBoogaloo");
+let sayHello2TokyoDrift = document.querySelector(".sayHello2TokyoDrift")
+
+
+setTimeout(() => {
+    home.style.display = 'flex';
+    home.style.animation = 'fade-in 1s';
+}, 4200);
+
+setTimeout(() => {
+    contact.style.display = 'flex';
+    contact.style.animation = 'fade-in 1s';
+}, 4300);
+
+setTimeout(() => {
+    bigD.style.display = 'flex';
+    bigD.style.animation = 'zoom-in 1s';
+}, 4200);
+
+setTimeout(() => {
+    childBanner1Heading.style.visibility = 'visible';
+    childBanner1Heading.style.animation = 'fade-up 1s';
+}, 4400);
+
+setTimeout(() => {
+    childBanner1Name.style.visibility = 'visible';
+    childBanner1Name.style.animation = 'fade-up 1s';
+}, 4550);
+
+setTimeout(() => {
+    iDevelop.style.visibility = 'visible';
+    iDevelop.style.animation = 'fade-up 1s';
+}, 4700);
+
+setTimeout(() => {
+    smallBio.style.display = 'flex';
+    smallBio.style.animation = 'fade-up 1s';
+}, 4850);
+
+setTimeout(() => {
+    resumeButton2ElectricBoogaloo.style.visibility = 'visible';
+    resumeButton2ElectricBoogaloo.style.animation = 'fade-up 1s';
+}, 5000);
+
+setTimeout(() => {
+    sayHello2TokyoDrift.style.visibility = 'visible';
+    sayHello2TokyoDrift.style.animation = 'fade-up 1s';
+}, 5150);
+
+
+
