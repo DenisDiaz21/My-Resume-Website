3a0144492fc0f7b39138b2e334891953a15a2abd
DenisDiaz21 <71994529+DenisDiaz21@users.noreply.github.com>
2021-05-20 17:20:16 -0400

    Fixed bug with AOS library where the projects tab wouldn't animate correctly.

--- Scripts/ProjectTabs.js
+++ Scripts/ProjectTabs.js
@@ -15,6 +15,8 @@ projectContent.forEach((e,i) => {
 button1.onclick = () =>{
     project1.style.display = 'flex';
     project1.style.animation = 'fade-in .5s'
+    // Refreshes library to fix scroll bug where it wouldn't flip
+    AOS.refresh();
 
     project2.style.display = 'none';
     project3.style.display = 'none';
@@ -25,6 +27,7 @@ button2.onclick = () =>{
 
     project2.style.display = 'flex';
     project2.style.animation = 'fade-in .5s'
+    AOS.refresh();
 
     project3.style.display = 'none';
 }
@@ -34,5 +37,5 @@ button3.onclick = () =>{
 
     project3.style.display = 'flex';
     project3.style.animation = 'fade-in .5s';
+    AOS.refresh();
 }
-
